8bf8e1cd79a392129deb9a1ba1e679a653d9c1f8
Philip Okanlawon <f.bobo@live.com>
2022-05-21 01:59:46 -0600

    change type of joke

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function tellMe(joke) {
 // Get Jokes from JokeAPI
 async function getJokes() {
   let joke = '';
-  const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+  const apiUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
